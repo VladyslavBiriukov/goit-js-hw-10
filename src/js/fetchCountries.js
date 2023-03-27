c63724ecdd6239c9e32b29a780cf49661d6fbf28
Vladyslav Biriukov <vladislavbiriukovsquirrel@gmail.com>
2023-03-27 20:56:12 +0300

    Update fetchCountries.js

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -7,6 +7,7 @@ export class CountriesAPI {
 fetchCountries (name) {
     return fetch(`${this.#BASE_URL}${name}?${this.#FIELDS}`)
         .then(response => {
+            // console.log(response);
             if (!response.ok) {
             throw new Error(response.status);
         };
