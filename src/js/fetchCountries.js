e468c930f446a75b7cbb7cc71f28fbb03556f2c9
Vladyslav Biriukov <vladislavbiriukovsquirrel@gmail.com>
2023-04-07 16:22:35 +0300

    Update fetchCountries.js

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -4,15 +4,14 @@ export class CountriesAPI {
     #BASE_URL = 'https://restcountries.com/v3.1/name/';
     #FIELDS = 'fields=name,capital,population,flags,languages'
     
-fetchCountries (name) {
-    return fetch(`${this.#BASE_URL}${name}?${this.#FIELDS}`)
-        .then(response => {
-            // console.log(response);
-            if (!response.ok) {
-            throw new Error(response.status);
-        };
-        return response.json();
-    });
+    async fetchCountries (name) {
+    const response = await fetch(`${this.#BASE_URL}${name}?${this.#FIELDS}`);
+    // console.log(response);
+    if (!response.ok) {
+        throw new Error(response.status);
+    }
+    ;
+    return await response.json();
     }
 }
 
